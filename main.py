93995f45ab4034b9c87cbfe21a65585c792ca969
ChrisTo0109 <70095389+ChrisTo0109@users.noreply.github.com>
2020-08-24 09:51:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-hello
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='p/')
+
+@bot.event
+async def on_ready():
+   print(">> Bot is online <<")
+
+@bot.event
+async def on_member_join(member):
+   channel = bot.get_channel(653534069061320714)
+   await channel.send(f'@{member} join!') 
+
+@bot.command()
+async def ping(ctx)
+
+bot.run('NzM4MzY3MzAzNDg1MjI3MTA4.XyK4Iw.N1r6gm1yqkSpNpgRMfA4zfd4S1Y')
